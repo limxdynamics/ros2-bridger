ea4cd8cc5e72cd4e50ccd22682857d7154a2308e
jenkins_ci <jenkins_ci@limxdynamics.com>
2026-08-12 21:21:52 +0800

    fix: Solve the issue where teleop_msgs/VRState cannot be bridged

--- amd64/foxy/install/teleop_msgs/share/teleop_msgs/cmake/ament_cmake_export_dependencies-extras.cmake
+++ amd64/foxy/install/teleop_msgs/share/teleop_msgs/cmake/ament_cmake_export_dependencies-extras.cmake
@@ -1,6 +1,6 @@
 # generated from ament_cmake_export_dependencies/cmake/ament_cmake_export_dependencies-extras.cmake.in
 
-set(_exported_dependencies "std_msgs;controller_msgs;std_msgs;builtin_interfaces;controller_msgs;rosidl_runtime_c;rosidl_typesupport_interface;std_msgs;builtin_interfaces;controller_msgs;std_msgs;builtin_interfaces;controller_msgs;rosidl_default_runtime")
+set(_exported_dependencies "std_msgs;geometry_msgs;controller_msgs;std_msgs;builtin_interfaces;geometry_msgs;controller_msgs;rosidl_runtime_c;rosidl_typesupport_interface;std_msgs;builtin_interfaces;geometry_msgs;controller_msgs;std_msgs;builtin_interfaces;geometry_msgs;controller_msgs;rosidl_default_runtime")
 
 find_package(ament_cmake_libraries QUIET REQUIRED)
 
